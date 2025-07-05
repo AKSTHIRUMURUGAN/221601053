9dddcfcd002a5cad04f48a899dbfab44489c92c7
AKSTHIRUMURUGAN <thirumuruganaks@gmail.com>
2025-07-05 12:42:25 +0530

    authentication
    only authentication

---
+++ backend-test-submission/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+app.get('/protected', require('./middleware/authMiddleware'), (req, res) => {
+  res.status(200).json({ message: `Hello User ${req.user}` });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
